757b8fb4ffe5aaba6e12719f2c8434a17502333e
Elena <aleklenoch@gmail.com>
2023-07-31 18:54:29 +0300

    #6 класс Outfit. В файле Outfit.cs описн класс подобранного ползователем комплекта одежды. Содержит наименование и коллекцию предметов одежды. Может содержть фотографию, путь к которой соджержит сворйство photo.

---
+++ wardrobe/Outfit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wardrobe
+{
+    internal class Outfit
+    {
+        public string outfit_name;
+        public ICollection <Clothes_Item> outfit;
+        public string? photo;
+    }
+}
