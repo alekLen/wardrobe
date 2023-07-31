d3aa7e4f37a512e60937bdec7b9bef6cd520545b
Elena <aleklenoch@gmail.com>
2023-07-31 18:02:14 +0300

    класс Clothes_type.
    в файле Clothes_type.cs создан класс Clothes_type.
    класс содержит автоматические свойства и конструктор.
    нужен для отображения типов ождежды.
    на его основе будет создана таблица типов ождежды в БД.

---
+++ wardrobe/Clothes_type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wardrobe
+{
+    internal class Clothes_type
+    {
+        public string Type_name { get; set; }
+        public Clothes_type(string name) => Type_name = name;
+    }
+}
