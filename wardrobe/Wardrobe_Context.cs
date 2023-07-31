93a5770cd6debe78d0b99f760b495a37e0590199
Elena <aleklenoch@gmail.com>
2023-07-31 22:24:56 +0300

    #14 создаем класс Wardrobe_Context
    наследует класс DbContext.
    необходим для работы модели с БД.
    тмеет 3 свойства: коллкцию предметов одежды,
    комплектов и альбомов с комплектами.
    будет создана или открыта база данных Wardrobe.

---
+++ wardrobe/Wardrobe_Context.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wardrobe
+{
+    internal class Wardrobe_Context : DbContext
+    {
+       
+        public Wardrobe_Context()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Clothes_Item> clothes_items { get; set; }
+        public DbSet<Outfit> outfits { get; set; }
+        public DbSet<Album> albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // метод UseLazyLoadingProxies() делает доступной ленивую загрузку.
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Server=DESKTOP-G30VB0K\MSSQLSERVER01;Database=Wardrobe;Integrated Security=SSPI;TrustServerCertificate=true;");
+        }
+    }
+}
