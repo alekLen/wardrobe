26959b6a66a9d28bda7086988e32d291998c50ba
Elena <aleklenoch@gmail.com>
2023-07-31 17:56:49 +0300

    класс Season.
    в файле Season.cs создан класс Season.
    класс содержит автоматические свойства и конструктор.
    нужен для отображения сезона ождежды.
    на его основе будет создана таблица сезонов в БД.

---
+++ wardrobe/Season.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wardrobe
+{
+    internal class Season
+    {
+        public string Season_name { get; set; }
+        public Season(string name) => Season_name = name;
+    }
+}
