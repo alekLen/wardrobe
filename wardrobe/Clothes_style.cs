3a52fffda1705c334359a60c34b6d7fa6898a308
Elena <aleklenoch@gmail.com>
2023-07-31 18:26:56 +0300

    #4 класс Clothes_style.
    в файле Clothes_style.cs создан класс Clothes_style.
    класс содержит автоматические свойства и конструктор.
    нужен для отображения стилей ождежды.
    на его основе будет создана таблица стилей в БД.

---
+++ wardrobe/Clothes_style.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wardrobe
+{
+    internal class Clothes_style
+    {
+        public string Style_name { get; set; }
+        public Clothes_style(string name) => Style_name = name;
+    }
+}
