82e0ddc3fbc51b2b459ad5a6c58a64358401f936
Elena <aleklenoch@gmail.com>
2023-07-31 18:13:22 +0300

    #3 класс Colors.
    в файле Colors.cs создан класс Colors.
    класс содержит автоматические свойства и конструктор.
    нужен для отображения цветов ождежды.
    на его основе будет создана таблица цветов в БД.

---
+++ wardrobe/Colors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wardrobe
+{
+    internal class Colors
+    {
+        public string Color_name { get; set; }
+        public Colors(string name) => Color_name = name;
+    }
+}
