d2d53c41550dc8b486686b246d9d909457297503
Elena <aleklenoch@gmail.com>
2023-07-31 19:00:01 +0300

    #7 класс Album. В файле Album.cs описн класс, обьединяющий подбранные ползователем комплекты одежды. Содержит наименование и коллекцию комплектов одежды.

---
+++ wardrobe/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wardrobe
+{
+    internal class Album
+    {
+        public string album_name;
+        public ICollection<Outfit> album;
+    }
+}
