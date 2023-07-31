ae6c581e2b9c5d92fba7f563d26803dd47176b62
Elena <aleklenoch@gmail.com>
2023-07-31 18:38:16 +0300

    #5 класс  Clothes_Item.
    в файле Clothes_Item.cs создан класс  Clothes_Item.
    класс описывает предмет одежды.
    сдержит наименование, цвет, стиль, тип одежды, сезон.
    может содержать дату и место покупки, размер.
    Свойство string photo будет содержать путь к фотографии.
    на его основе будет создана таблица цветов в БД.

---
+++ wardrobe/Clothes_Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wardrobe
+{
+    internal class Clothes_Item
+    {
+        public string Clothes_Item_name { get; set; }
+        public Colors color;
+        public Clothes_type type;
+        public Season season;
+        public Clothes_style style;
+        public DateTime? date;
+        public string? place;
+        public string? size;
+        public string photo;
+    }
+}
