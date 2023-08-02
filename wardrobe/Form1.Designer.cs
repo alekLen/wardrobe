fd8e805944bce4f60655f5d26b119a9ac712fc43
Elena <aleklenoch@gmail.com>
2023-08-02 18:31:28 +0300

    #24 дизайн главного окна
    главное окно приложения отображает все одежду имеющуюся в гардеробе.
    позволяет создавать комплекты одежды.
    ставить фильтры просмотра.

--- wardrobe/Form1.Designer.cs
+++ wardrobe/Form1.Designer.cs
@@ -28,12 +28,471 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listView1 = new ListView();
+            pictureBox1 = new PictureBox();
+            menuStrip1 = new MenuStrip();
+            редактироватьToolStripMenuItem = new ToolStripMenuItem();
+            стильОдеждыToolStripMenuItem = new ToolStripMenuItem();
+            добавитьToolStripMenuItem = new ToolStripMenuItem();
+            удалитьToolStripMenuItem = new ToolStripMenuItem();
+            редактироватьToolStripMenuItem1 = new ToolStripMenuItem();
+            сезонToolStripMenuItem = new ToolStripMenuItem();
+            цветToolStripMenuItem = new ToolStripMenuItem();
+            альбомToolStripMenuItem = new ToolStripMenuItem();
+            просмотрToolStripMenuItem = new ToolStripMenuItem();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            listBox1 = new ListBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            listBox4 = new ListBox();
+            listView2 = new ListView();
+            listView3 = new ListView();
+            listView4 = new ListView();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            button4 = new Button();
+            listBox5 = new ListBox();
+            label1 = new Label();
+            listBox6 = new ListBox();
+            listBox7 = new ListBox();
+            label2 = new Label();
+            label3 = new Label();
+            listView5 = new ListView();
+            label4 = new Label();
+            button5 = new Button();
+            button6 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(205, 378);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(261, 147);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.List;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(205, 31);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(261, 272);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { редактироватьToolStripMenuItem, просмотрToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1482, 28);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // редактироватьToolStripMenuItem
+            // 
+            редактироватьToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { стильОдеждыToolStripMenuItem, сезонToolStripMenuItem, цветToolStripMenuItem, альбомToolStripMenuItem });
+            редактироватьToolStripMenuItem.Name = "редактироватьToolStripMenuItem";
+            редактироватьToolStripMenuItem.Size = new Size(126, 24);
+            редактироватьToolStripMenuItem.Text = "редактировать";
+            // 
+            // стильОдеждыToolStripMenuItem
+            // 
+            стильОдеждыToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { добавитьToolStripMenuItem, удалитьToolStripMenuItem, редактироватьToolStripMenuItem1 });
+            стильОдеждыToolStripMenuItem.Name = "стильОдеждыToolStripMenuItem";
+            стильОдеждыToolStripMenuItem.Size = new Size(210, 26);
+            стильОдеждыToolStripMenuItem.Text = "характеристики";
+            // 
+            // добавитьToolStripMenuItem
+            // 
+            добавитьToolStripMenuItem.Name = "добавитьToolStripMenuItem";
+            добавитьToolStripMenuItem.Size = new Size(132, 26);
+            добавитьToolStripMenuItem.Text = "стиль";
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            удалитьToolStripMenuItem.Size = new Size(132, 26);
+            удалитьToolStripMenuItem.Text = "сезон";
+            // 
+            // редактироватьToolStripMenuItem1
+            // 
+            редактироватьToolStripMenuItem1.Name = "редактироватьToolStripMenuItem1";
+            редактироватьToolStripMenuItem1.Size = new Size(132, 26);
+            редактироватьToolStripMenuItem1.Text = "цвет";
+            // 
+            // сезонToolStripMenuItem
+            // 
+            сезонToolStripMenuItem.Name = "сезонToolStripMenuItem";
+            сезонToolStripMenuItem.Size = new Size(210, 26);
+            сезонToolStripMenuItem.Text = "предмет одежды";
+            // 
+            // цветToolStripMenuItem
+            // 
+            цветToolStripMenuItem.Name = "цветToolStripMenuItem";
+            цветToolStripMenuItem.Size = new Size(210, 26);
+            цветToolStripMenuItem.Text = "комплект";
+            // 
+            // альбомToolStripMenuItem
+            // 
+            альбомToolStripMenuItem.Name = "альбомToolStripMenuItem";
+            альбомToolStripMenuItem.Size = new Size(210, 26);
+            альбомToolStripMenuItem.Text = "альбом";
+            // 
+            // просмотрToolStripMenuItem
+            // 
+            просмотрToolStripMenuItem.Name = "просмотрToolStripMenuItem";
+            просмотрToolStripMenuItem.Size = new Size(168, 24);
+            просмотрToolStripMenuItem.Text = "просмотр_альбомов";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(349, 531);
+            button1.Name = "button1";
+            button1.Size = new Size(261, 29);
+            button1.TabIndex = 3;
+            button1.Text = "цветовая статистика";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(673, 531);
+            button2.Name = "button2";
+            button2.Size = new Size(261, 29);
+            button2.TabIndex = 4;
+            button2.Text = "статистика видов одежды";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(994, 531);
+            button3.Name = "button3";
+            button3.Size = new Size(261, 29);
+            button3.TabIndex = 5;
+            button3.Text = "статистика стилей";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(205, 336);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(190, 24);
+            listBox1.TabIndex = 6;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(510, 31);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(261, 272);
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(794, 31);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(261, 272);
+            pictureBox3.TabIndex = 8;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(1077, 31);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(261, 272);
+            pictureBox4.TabIndex = 9;
+            pictureBox4.TabStop = false;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 20;
+            listBox2.Location = new Point(510, 336);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(199, 24);
+            listBox2.TabIndex = 10;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 20;
+            listBox3.Location = new Point(794, 336);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(210, 24);
+            listBox3.TabIndex = 11;
+            // 
+            // listBox4
+            // 
+            listBox4.FormattingEnabled = true;
+            listBox4.ItemHeight = 20;
+            listBox4.Location = new Point(1077, 336);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(190, 24);
+            listBox4.TabIndex = 12;
+            // 
+            // listView2
+            // 
+            listView2.Location = new Point(510, 378);
+            listView2.Name = "listView2";
+            listView2.Size = new Size(261, 147);
+            listView2.TabIndex = 13;
+            listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView3
+            // 
+            listView3.Location = new Point(794, 378);
+            listView3.Name = "listView3";
+            listView3.Size = new Size(261, 147);
+            listView3.TabIndex = 14;
+            listView3.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView4
+            // 
+            listView4.Location = new Point(1077, 378);
+            listView4.Name = "listView4";
+            listView4.Size = new Size(261, 147);
+            listView4.TabIndex = 15;
+            listView4.UseCompatibleStateImageBehavior = false;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(433, 343);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(18, 17);
+            checkBox1.TabIndex = 16;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(734, 343);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(18, 17);
+            checkBox2.TabIndex = 17;
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(1026, 343);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(18, 17);
+            checkBox3.TabIndex = 18;
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(1301, 343);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(18, 17);
+            checkBox4.TabIndex = 19;
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1376, 276);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 113);
+            button4.TabIndex = 20;
+            button4.Text = "добавить комплект";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // listBox5
+            // 
+            listBox5.FormattingEnabled = true;
+            listBox5.ItemHeight = 20;
+            listBox5.Location = new Point(32, 96);
+            listBox5.Name = "listBox5";
+            listBox5.Size = new Size(150, 24);
+            listBox5.TabIndex = 21;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(32, 73);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 20);
+            label1.TabIndex = 22;
+            label1.Text = "цвет";
+            // 
+            // listBox6
+            // 
+            listBox6.FormattingEnabled = true;
+            listBox6.ItemHeight = 20;
+            listBox6.Location = new Point(32, 169);
+            listBox6.Name = "listBox6";
+            listBox6.Size = new Size(150, 24);
+            listBox6.TabIndex = 23;
+            // 
+            // listBox7
+            // 
+            listBox7.FormattingEnabled = true;
+            listBox7.ItemHeight = 20;
+            listBox7.Location = new Point(32, 233);
+            listBox7.Name = "listBox7";
+            listBox7.Size = new Size(150, 24);
+            listBox7.TabIndex = 24;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(32, 137);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 20);
+            label2.TabIndex = 25;
+            label2.Text = "сезон";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(37, 205);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 20);
+            label3.TabIndex = 26;
+            label3.Text = "стиль";
+            // 
+            // listView5
+            // 
+            listView5.Location = new Point(32, 299);
+            listView5.Name = "listView5";
+            listView5.Size = new Size(148, 161);
+            listView5.TabIndex = 27;
+            listView5.UseCompatibleStateImageBehavior = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(35, 276);
+            label4.Name = "label4";
+            label4.Size = new Size(59, 20);
+            label4.TabIndex = 28;
+            label4.Text = "фильтр";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(35, 531);
+            button5.Name = "button5";
+            button5.Size = new Size(145, 29);
+            button5.TabIndex = 29;
+            button5.Text = "сбросить фильтр";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(32, 477);
+            button6.Name = "button6";
+            button6.Size = new Size(148, 29);
+            button6.TabIndex = 30;
+            button6.Text = "фильтровать";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1482, 572);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(label4);
+            Controls.Add(listView5);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(listBox7);
+            Controls.Add(listBox6);
+            Controls.Add(label1);
+            Controls.Add(listBox5);
+            Controls.Add(button4);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(listView4);
+            Controls.Add(listView3);
+            Controls.Add(listView2);
+            Controls.Add(listBox4);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(listBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(listView1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Гардероб";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListView listView1;
+        private PictureBox pictureBox1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem просмотрToolStripMenuItem;
+        private ToolStripMenuItem редактироватьToolStripMenuItem;
+        private ToolStripMenuItem стильОдеждыToolStripMenuItem;
+        private ToolStripMenuItem добавитьToolStripMenuItem;
+        private ToolStripMenuItem удалитьToolStripMenuItem;
+        private ToolStripMenuItem редактироватьToolStripMenuItem1;
+        private ToolStripMenuItem сезонToolStripMenuItem;
+        private ToolStripMenuItem цветToolStripMenuItem;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private ListBox listBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private ListBox listBox4;
+        private ListView listView2;
+        private ListView listView3;
+        private ListView listView4;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private Button button4;
+        private ListBox listBox5;
+        private Label label1;
+        private ListBox listBox6;
+        private ListBox listBox7;
+        private Label label2;
+        private Label label3;
+        private ListView listView5;
+        private Label label4;
+        private Button button5;
+        private ToolStripMenuItem альбомToolStripMenuItem;
+        private Button button6;
     }
 }
