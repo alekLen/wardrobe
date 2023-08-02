3da833a7271ff195acf31836bb2143ffaa528a77
Elena <aleklenoch@gmail.com>
2023-08-02 12:20:38 +0300

    #16 изменения в классе Wardrobe_Context
    добавили создание таблиц clothes_styles, clothes_types, seasons, colors.
    в меоде OnConfiguring убрали строчку с настройками.
    теперь настройки будут передаваться в отдельном файле в json формате.

--- wardrobe/Wardrobe_Context.cs
+++ wardrobe/Wardrobe_Context.cs
@@ -9,20 +9,34 @@ namespace wardrobe
 {
     internal class Wardrobe_Context : DbContext
     {
-       
-        public Wardrobe_Context()
-        {
-            Database.EnsureCreated();
-        }
+        // Для работы с БД MS SQL Server необходимо добавить пакет:
+        // Microsoft.EntityFrameworkCore.SqlServer(представляет функциональность Entity Framework для работы с MS SQL Server)
 
+        // Microsoft.Extensions.Configuration.Json. Этот пакет специально предназначен для работы с конфигурацией в формате json.
+
+        public Wardrobe_Context(DbContextOptions<Wardrobe_Context> options)
+           : base(options)
+        {
+            if (Database.EnsureCreated())
+            {
+                Clothes_style style1 = new Clothes_style {Style_name = "Повседневный" };             
+
+                clothes_styles.Add(style1);
+              
+                SaveChanges();
+            }
+        }
+        public DbSet<Clothes_style> clothes_styles { get; set; }
+        public DbSet<Clothes_type> clothes_types { get; set; }
+        public DbSet<Season> seasons { get; set; }
+        public DbSet<Colors> colors { get; set; }
         public DbSet<Clothes_Item> clothes_items { get; set; }
         public DbSet<Outfit> outfits { get; set; }
         public DbSet<Album> albums { get; set; }
 
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            // метод UseLazyLoadingProxies() делает доступной ленивую загрузку.
-            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Server=DESKTOP-G30VB0K\MSSQLSERVER01;Database=Wardrobe;Integrated Security=SSPI;TrustServerCertificate=true;");
+        {         
         }
     }
 }
