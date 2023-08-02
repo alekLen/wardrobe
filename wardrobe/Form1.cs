293011263f0e10a972b5204ccb914905ef83896c
Elena <aleklenoch@gmail.com>
2023-08-02 19:16:09 +0300

    #28 Form1 реализует интерфейс IForm1
    в главное окно добавляются цвет, стиль и сезон.

--- wardrobe/Form1.cs
+++ wardrobe/Form1.cs
@@ -1,10 +1,26 @@
 namespace wardrobe
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form,IForm1
     {
         public Form1()
         {
             InitializeComponent();
+        }
+        public void SetSeasonToWardrobe(string s)
+        {
+            listBox6.Items.Add(s);
+        }
+        public void SetStyleToWardrobe(string s)
+        {
+            listBox7.Items.Add(s);
+        }
+        public void SetTypeToWardrobe(string s)
+        {
+
+        }
+        public void SetColorToWardrobe(string s)
+        {
+            listBox5.Items.Add(s);
         }
     }
 }
