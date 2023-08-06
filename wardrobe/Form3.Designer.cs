01eaae9c89389c0ea9cfb259e6352a8ae18ccb62
Elena <aleklenoch@gmail.com>
2023-08-06 19:14:30 +0300

    #71 дизайн Form3
    добавлены отображающиеся характеристики

--- wardrobe/Form3.Designer.cs
+++ wardrobe/Form3.Designer.cs
@@ -34,60 +34,157 @@
             textBoxColor = new TextBox();
             textBoxSeason = new TextBox();
             button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label6 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // textBoxName
             // 
-            textBoxName.Location = new Point(53, 44);
+            textBoxName.Enabled = false;
+            textBoxName.Location = new Point(83, 12);
             textBoxName.Name = "textBoxName";
-            textBoxName.Size = new Size(234, 27);
+            textBoxName.Size = new Size(271, 27);
             textBoxName.TabIndex = 0;
             // 
             // pictureBox1
             // 
             pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            pictureBox1.Location = new Point(327, 12);
+            pictureBox1.Location = new Point(360, 12);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(419, 415);
+            pictureBox1.Size = new Size(708, 653);
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             // 
             // textBoxStyle
             // 
-            textBoxStyle.Location = new Point(55, 98);
+            textBoxStyle.Enabled = false;
+            textBoxStyle.Location = new Point(83, 55);
             textBoxStyle.Name = "textBoxStyle";
-            textBoxStyle.Size = new Size(232, 27);
+            textBoxStyle.Size = new Size(271, 27);
             textBoxStyle.TabIndex = 2;
             // 
             // textBoxColor
             // 
-            textBoxColor.Location = new Point(55, 159);
+            textBoxColor.Enabled = false;
+            textBoxColor.Location = new Point(83, 96);
             textBoxColor.Name = "textBoxColor";
-            textBoxColor.Size = new Size(232, 27);
+            textBoxColor.Size = new Size(271, 27);
             textBoxColor.TabIndex = 3;
             // 
             // textBoxSeason
             // 
-            textBoxSeason.Location = new Point(55, 220);
+            textBoxSeason.Enabled = false;
+            textBoxSeason.Location = new Point(83, 145);
             textBoxSeason.Name = "textBoxSeason";
-            textBoxSeason.Size = new Size(232, 27);
+            textBoxSeason.Size = new Size(271, 27);
             textBoxSeason.TabIndex = 4;
             // 
             // button1
             // 
-            button1.Location = new Point(55, 390);
+            button1.Location = new Point(63, 495);
             button1.Name = "button1";
-            button1.Size = new Size(232, 29);
+            button1.Size = new Size(232, 43);
             button1.TabIndex = 5;
             button1.Text = "добвить в комплект";
             button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(21, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 20);
+            label1.TabIndex = 6;
+            label1.Text = "стиль";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 99);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 20);
+            label2.TabIndex = 7;
+            label2.Text = "цвет";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(21, 148);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 20);
+            label3.TabIndex = 8;
+            label3.Text = "сезон";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(83, 191);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(271, 27);
+            textBox1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(21, 194);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 20);
+            label4.TabIndex = 10;
+            label4.Text = "дата";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 237);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 20);
+            label5.TabIndex = 11;
+            label5.Text = "магазин";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(83, 237);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(271, 27);
+            textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(229, 280);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 13;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(9, 287);
+            label6.Name = "label6";
+            label6.Size = new Size(61, 20);
+            label6.TabIndex = 14;
+            label6.Text = "размер";
             // 
             // Form3
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(777, 450);
+            ClientSize = new Size(1080, 688);
+            Controls.Add(label6);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(textBoxSeason);
             Controls.Add(textBoxColor);
@@ -109,5 +206,14 @@
         private TextBox textBoxColor;
         private TextBox textBoxSeason;
         private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label6;
     }
 }
