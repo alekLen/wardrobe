cbf28fe84da1f4dcec69470bdf17a9a2e7e76fa8
Elena <aleklenoch@gmail.com>
2023-08-13 18:37:41 +0300

    #149 Form5 выставляет подписку на событие загрузки формы получает пути к фотографиям вещей из комплекта

--- wardrobe/Form5.cs
+++ wardrobe/Form5.cs
@@ -13,6 +13,9 @@ namespace wardrobe
     public partial class Form5 : Form
     {
         public Form1 MainForm { get; set; }
+        public string Path { get; set; }
+        public int Id_Item { get; set; }
+        public event EventHandler<EventArgs> TakePhoto;
         public Form5()
         {
             InitializeComponent();
@@ -20,19 +23,24 @@ namespace wardrobe
 
         private void LoadF5(object sender, EventArgs e)
         {
-            for (int i = 0; i < 5; i++)
+            int a=MainForm.Ids.Count;
+            for (int i = 0; i < a; i++)
             {
                 PictureBox pictureBox = new PictureBox();
                 pictureBox.Width = 200;
                 pictureBox.Height = 200;
                 pictureBox.Location = new System.Drawing.Point(20 + i * 220, 100);
                 pictureBox.BackColor = System.Drawing.Color.Gray;
-                pictureBox.Image = Image.FromFile("Photos/up.png");
+                // pictureBox.Image = Image.FromFile("Photos/up.png");
+                Id_Item = MainForm.Ids[i];
+                TakePhoto?.Invoke(this, new EventArgs());
+                pictureBox.Image = Image.FromFile(Path);
                 pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
                 this.Controls.Add(pictureBox);
 
             }
-            this.Width = 5 * 220 + 100;
-        }
+            this.Width = a * 220 + 100;
+        }       
+       
     }
 }
